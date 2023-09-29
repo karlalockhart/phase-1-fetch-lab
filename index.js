abaf3f86a8991baee6b9c2d263f5c91c032e424c
Karla Lockhart <kblockhart@gmail.com>
2023-09-29 12:23:53 -0700

    made changes to index.js &  completed phase-1-fetch-lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function fetchBooks() {
+function fetchBooks(){
+  return fetch('https://anapioficeandfire.com/api/books')
+      .then((resp) => resp.json())
+      //.then((json) => console.log(json)); 
+      .then(json => renderBooks(json))
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
@@ -15,3 +18,25 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+/*
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
+
+function renderBooks(books) {
+  const main = document.querySelector('main')
+  books.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
+*/
